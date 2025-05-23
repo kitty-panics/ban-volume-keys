8bef011da363d9e0081209042f10fefbdff37d10
kitty-panics <e222d29f@outlook.com>
2025-05-23 12:21:12 -0400

    fix: Update pack.sh

--- pack.sh
+++ pack.sh
@@ -6,6 +6,9 @@ MOD_ALL_DIR="./Ban-Volume-All/system/usr/keylayout"
 rm $MOD_UP_DIR/*
 rm $MOD_DOWN_DIR/*
 rm $MOD_ALL_DIR/*
+rm $MOD_UP_DIR/.*
+rm $MOD_DOWN_DIR/.*
+rm $MOD_ALL_DIR/.*
 
 SYS_UP_DIR="./sys_ban_volume_up"
 SYS_DOWN_DIR="./sys_ban_volume_down"
@@ -13,9 +16,9 @@ SYS_ALL_DIR="./sys_ban_volume_all"
 cp $SYS_UP_DIR/*   $MOD_UP_DIR
 cp $SYS_DOWN_DIR/* $MOD_DOWN_DIR
 cp $SYS_ALL_DIR/*  $MOD_ALL_DIR
-#rm -r $SYS_UP_DIR
-#rm -r $SYS_DOWN_DIR
-#rm -r $SYS_ALL_DIR
+rm -r $SYS_UP_DIR
+rm -r $SYS_DOWN_DIR
+rm -r $SYS_ALL_DIR
 
 cd Ban-Volume-Up; zip -r ../Magisk-BAN_VOLUME_UP.zip *; cd ..
 cd Ban-Volume-Down; zip -r ../Magisk-BAN_VOLUME_DOWN.zip *; cd ..
